e65c4de84ba1e1aa63e1aa996cfe674038c2a721
Phuong Hau <hvphuong98@gmail.com>
2021-05-23 12:24:20 +0700

    finished binding services to FindLowerNearestPrime handler

--- pkg/rest/prime.go
+++ pkg/rest/prime.go
@@ -1,9 +1,12 @@
 package rest
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/phuonghau98/stably-togo/pkg/service/prime"
 )
 
 type PrimeHandler struct{}
@@ -16,7 +19,43 @@ func (ph *PrimeHandler) Register(router *mux.Router) {
 	router.HandleFunc("/api/v1/prime/findnearest", ph.FindNearestPrime).Methods(http.MethodPost)
 }
 
+type findNearestPrimeBodyRequest struct {
+	Num int `json:"num",omitempty`
+}
+
+func (f findNearestPrimeBodyRequest) validate() error {
+	if f.Num <= 1 {
+		return fmt.Errorf("Input number should be larger than 1")
+	}
+	return nil
+}
+
+type findNearestPrimeBodyResponse struct {
+	Num int `json:"num"`
+}
+
 func (handler PrimeHandler) FindNearestPrime(w http.ResponseWriter, r *http.Request) {
-	writeErrorJSONResponse(w, "Not implemented yet", 500)
+	// Parse request body
+	var reqBody findNearestPrimeBodyRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		writeErrorJSONResponse(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	// validate inputs
+	if err := reqBody.validate(); err != nil {
+		writeErrorJSONResponse(w, err.Error(), 400)
+		return
+	}
+
+	// Process the request
+	foundPrimeNumber, err := prime.FindLowerNearestPrimeNumber(reqBody.Num)
+	if err != nil {
+		writeErrorJSONResponse(w, fmt.Sprintf("%v", foundPrimeNumber), 500)
+		return
+	}
+
+	writeSuccessJSONResponse(w, &findNearestPrimeBodyResponse{Num: foundPrimeNumber}, 200)
 	return
 }
