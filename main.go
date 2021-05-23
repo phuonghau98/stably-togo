85c7f892e0f009931b014902f7a315dab799532c
Phuong Hau <hvphuong98@gmail.com>
2021-05-23 21:12:18 +0700

    make server configurable using environment variables

--- main.go
+++ main.go
@@ -6,39 +6,64 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/phuonghau98/stably-togo/pkg/rest"
+	"github.com/phuonghau98/stably-togo/pkg/util"
 	"github.com/rs/cors"
 )
 
 var (
-	HTTP_BINDING_ADDR = ":8080"
-	ENV               = "development"
+	HTTP_BINDING_ADDR                  = ":8080"
+	ENV                                = "development"
+	DOT_ENV_FILENAME                   = ".env"
+	UI_BUILD_FOLDER_KEY                = "UI_BUILD_FOLDER"
+	UI_BUILD_INDEX_FILENAME_KEY        = "UI_BUILD_INDEX_FILENAME"
+	UI_CLIENT_URL_KEY                  = "UI_CLIENT_URL"
+	ENV_KEY                            = "ENV"
+	ENV_PRODUCTION                     = "production"
+	ENV_DEVELOPMENT                    = "development"
+	DEFAULT_UI_BUILD_FOLDER_PATH       = "ui/build"
+	DEFAULT_UI_BUILD_INDEX_FILENAME    = "index.html"
+	DEFAULT_CLIENT_URL                 = "http://localhost:3000"
+	DEFAULT_ENV                        = "development"
+	HTTP_SERVER_WRITE_TIMEOUT_DURATION = 30 * time.Second
+	HTTP_SERVER_READ_TIMEOUT_DURATION  = 30 * time.Second
 )
 
 func main() {
+	// Load dot env file
+	err := godotenv.Load(DOT_ENV_FILENAME)
+	if err != nil {
+		log.Println("No .env file detected")
+	}
 
+	// env variables
+	uiBuildFolderPath := util.Getenv(UI_BUILD_FOLDER_KEY, DEFAULT_UI_BUILD_FOLDER_PATH)
+	uiIndexFilename := util.Getenv(UI_BUILD_INDEX_FILENAME_KEY, DEFAULT_UI_BUILD_INDEX_FILENAME)
+	uiClientURL := util.Getenv(UI_CLIENT_URL_KEY, DEFAULT_CLIENT_URL)
+	env := util.Getenv(ENV_KEY, ENV_DEVELOPMENT)
+	// Init mux router
 	r := mux.NewRouter()
 	primeHandler := rest.NewPrimeHandler()
 	primeHandler.Register(r)
 
 	// Handle react build in production
-	spa := rest.SPAHandler{StaticPath: "ui/build", IndexPath: "index.html"}
+	spa := rest.SPAHandler{StaticPath: uiBuildFolderPath, IndexPath: uiIndexFilename}
 	r.PathPrefix("/").Handler(spa)
 
 	// Cors
 	corsOptions := cors.Options{}
-	if ENV != "production" {
-		corsOptions.AllowedOrigins = []string{"http://localhost:3000"}
+	if env != ENV_PRODUCTION {
+		corsOptions.AllowedOrigins = []string{uiClientURL}
 	}
 
 	corsInstance := cors.New(corsOptions)
 
 	srv := &http.Server{
-		Handler: corsInstance.Handler(r),
-		Addr:    HTTP_BINDING_ADDR,
-		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		Handler:      corsInstance.Handler(r),
+		Addr:         HTTP_BINDING_ADDR,
+		WriteTimeout: HTTP_SERVER_WRITE_TIMEOUT_DURATION,
+		ReadTimeout:  HTTP_SERVER_READ_TIMEOUT_DURATION,
 	}
 
 	log.Println("Binding http server to ", HTTP_BINDING_ADDR)
