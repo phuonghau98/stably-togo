a8a178222ed4461b77279f98b4931b48ac4e6645
Phuong Hau <hvphuong98@gmail.com>
2021-05-23 15:21:47 +0700

    install AntDesign UI lib

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Form, Input } from 'antd';
+import './App.css'
+
 function App() {
   return (
     <div className="App">
-
+      <Form>
+        <Form.Item label="Enter a number that you want to find the lower nearest prime">
+          <Input />
+        </Form.Item>
+      </Form>
     </div>
   );
 }
