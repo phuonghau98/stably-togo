c787232a0bf208b0ec1912d549e9ce8915ea78fa
Phuong Hau <hvphuong98@gmail.com>
2021-05-23 11:23:09 +0700

    add simple algorithm to find highest prime number lower than the input number

--- pkg/service/prime/prime.go
+++ pkg/service/prime/prime.go
@@ -1,1 +1,22 @@
 package prime
+
+func isPrimeNumber(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func NearestPrimeNumber(n int) (error, int) {
+	for i := n - 1; i > 1; i-- {
+		if isPrimeNumber(i) {
+			return nil, i
+		}
+	}
+	return nil, -1
+}
