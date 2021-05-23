48dc4105a7b60a3e202c537fe4f61c73044fdc35
Phuong Hau <hvphuong98@gmail.com>
2021-05-23 15:17:55 +0700

    add blank react app with typescript

---
+++ ui/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Form, Input } from 'antd';
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
