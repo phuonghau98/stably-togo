af6358aac00bbf72b7363967ce72346b9a818988
Phuong Hau <hvphuong98@gmail.com>
2021-05-23 15:45:06 +0700

    add form elements and validation for input

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Form, Input } from 'antd';
-import './App.css'
+import { Button, Col, Form, InputNumber, Row } from 'antd';
+import './App.css';
 
 function App() {
+  function handleFormSubmmited(values: any) {
+    console.log(values);
+  }
+
   return (
     <div className="App">
-      <Form>
-        <Form.Item label="Enter a number that you want to find the lower nearest prime">
-          <Input />
-        </Form.Item>
-      </Form>
+      <Row justify="center" align="middle" style={{ height: '100%' }}>
+        <Col>
+          <Form layout="vertical" onFinish={handleFormSubmmited}>
+            <Form.Item
+              label="Enter a number that you want to find the lower nearest prime"
+              name="num"
+              initialValue={2}
+              rules={[
+                {
+                  required: true,
+                  message: 'A number is required',
+                },
+              ]}
+            >
+              <InputNumber
+                min={2}
+                size="large"
+                style={{ width: '100%' }}
+                step={1}
+                parser={(v) => parseInt(v || '0')}
+              />
+            </Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              size="large"
+              style={{ width: '100%' }}
+            >
+              Find
+            </Button>
+          </Form>
+        </Col>
+      </Row>
     </div>
   );
 }
