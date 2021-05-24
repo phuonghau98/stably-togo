22aabba1771a7614d7832f45e38a8a01da27117c
Phuong Hau <hvphuong98@gmail.com>
2021-05-24 10:04:15 +0700

    add tests for /api/v2/prime/findnearest

--- pkg/rest/prime_test.go
+++ pkg/rest/prime_test.go
@@ -117,3 +117,103 @@ func TestFindLowerNearestPrimeHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestFindLowerNearestPrimeHandlerV2(t *testing.T) {
+	// Mock server
+	router := mux.NewRouter()
+	primeHandler := NewPrimeHandler()
+	primeHandler.Register(router)
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	// Utils
+	newreq := func(method, url string, bodyPayload interface{}) *http.Request {
+		bodyJSON, err := json.Marshal(bodyPayload)
+		if err != nil {
+			log.Fatalf("Cannot prepare post body: %v", err)
+		}
+		r, err := http.NewRequest(method, url, bytes.NewBuffer(bodyJSON))
+		if err != nil {
+			t.Fatal(err)
+		}
+		return r
+	}
+
+	testCases := []struct {
+		name        string
+		request     *http.Request
+		err         error
+		responseNum int
+	}{
+		{
+			name:        "Should return body contains `num: 11` as response",
+			request:     newreq(http.MethodPost, ts.URL+"/api/v2/prime/findnearest", findNearestPrimeBodyRequest{Num: 12}),
+			err:         nil,
+			responseNum: 11,
+		},
+		{
+			name:        "Should return body contains `num: -1` as response",
+			request:     newreq(http.MethodPost, ts.URL+"/api/v2/prime/findnearest", findNearestPrimeBodyRequest{Num: 2}),
+			err:         nil,
+			responseNum: -1,
+		},
+		{
+			name:        "Should return body contains `num: 1000000395723132233` as response ",
+			request:     newreq(http.MethodPost, ts.URL+"/api/v2/prime/findnearest", findNearestPrimeBodyRequest{Num: 1000000395723132283}),
+			err:         nil,
+			responseNum: 1000000395723132233,
+		},
+		{
+			name: "Should return `invalid request` error as response if num provided with string format",
+			request: newreq(http.MethodPost, ts.URL+"/api/v2/prime/findnearest", struct {
+				Num string `json:"num"`
+			}{Num: "23"}),
+			err: ErrPrimeInvalidInputFormat,
+		},
+		{
+			name: "Should return `invalid request` error as response if num provided with object format",
+			request: newreq(http.MethodPost, ts.URL+"/api/v2/prime/findnearest", struct {
+				Num string `json:"num"`
+			}{Num: "{}"}),
+			err: ErrPrimeInvalidInputFormat,
+		},
+		{
+			name:    "Should return `ErrPrimeInputNumberOutOfRange` error as response if num provided less than 2",
+			request: newreq(http.MethodPost, ts.URL+"/api/v2/prime/findnearest", findNearestPrimeBodyRequest{Num: -1}),
+			err:     ErrPrimeInputNumberOutOfRange,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Parse body
+			resp, err := http.DefaultClient.Do(testCase.request)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			var response findLowerNearestResponse
+			err = json.Unmarshal(body, &response)
+			if err != nil {
+				t.Fatalf("Cannot parse response body: %v", err)
+			}
+
+			// Assert error case
+			if testCase.err != nil {
+				wantedError := testCase.err.Error()
+				gotError := response.Error
+				if wantedError != gotError {
+					log.Fatalf("Should receive error: %v, but got: %v", wantedError, gotError)
+				}
+			} else {
+				// Assert valid case
+				wantedNum := testCase.responseNum
+				gotNum := response.Data.Num
+
+				if strconv.Itoa(wantedNum) != gotNum {
+					log.Fatalf("Should receive num: %v as response, but got: %v", wantedNum, gotNum)
+				}
+			}
+		})
+	}
+}
