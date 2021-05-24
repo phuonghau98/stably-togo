d8ec7c590b42c573f67f9c6c86d2c8ec8558408d
Phuong Hau <hvphuong98@gmail.com>
2021-05-24 09:30:37 +0700

    add an improved version of isPrimeNumber so that it could bigger number

--- pkg/service/prime/prime.go
+++ pkg/service/prime/prime.go
@@ -1,5 +1,9 @@
 package prime
 
+import (
+	"math"
+)
+
 func isPrimeNumber(n int) bool {
 	if n <= 1 {
 		return false
@@ -12,9 +16,28 @@ func isPrimeNumber(n int) bool {
 	return true
 }
 
+func isPrimeNumberV2(n int) bool {
+	for i := 2; i <= int(math.Floor(math.Sqrt(float64(n)))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return n > 1
+}
+
 func FindLowerNearestPrimeNumber(n int) (int, error) {
 	for i := n - 1; i > 1; i-- {
 		if isPrimeNumber(i) {
+			return i, nil
+		}
+	}
+	return -1, nil
+}
+
+func FindLowerNearestPrimeNumberV2(n int) (int, error) {
+	for i := n - 1; i > 1; i-- {
+		// big.NewInt(int64(i)).ProbablyPrime(0)
+		if isPrimeNumberV2(i) {
 			return i, nil
 		}
 	}
