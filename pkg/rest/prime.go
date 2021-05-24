a6121f1c234b72324cfe475e147a923b73a61a40
Phuong Hau <hvphuong98@gmail.com>
2021-05-24 10:03:48 +0700

    link /api/v2/prime/findnearest to FindLowerNearestPrimeNumberV2

--- pkg/rest/prime.go
+++ pkg/rest/prime.go
@@ -48,6 +48,7 @@ func NewPrimeHandler() *PrimeHandler {
 
 func (ph *PrimeHandler) Register(router *mux.Router) {
 	router.HandleFunc("/api/v1/prime/findnearest", ph.FindLowerNearestPrimeV1).Methods(http.MethodPost)
+	router.HandleFunc("/api/v2/prime/findnearest", ph.FindLowerNearestPrimeV2).Methods(http.MethodPost)
 }
 
 func (handler *PrimeHandler) FindLowerNearestPrimeV1(w http.ResponseWriter, r *http.Request) {
@@ -67,6 +68,32 @@ func (handler *PrimeHandler) FindLowerNearestPrimeV1(w http.ResponseWriter, r *h
 
 	// Process the request
 	foundPrimeNumber, err := prime.FindLowerNearestPrimeNumber(reqBody.Num)
+	if err != nil {
+		writeErrorJSONResponse(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writeSuccessJSONResponse(w, &findNearestPrimeBodyResponse{Num: strconv.Itoa(foundPrimeNumber)}, 200)
+	return
+}
+
+func (handler *PrimeHandler) FindLowerNearestPrimeV2(w http.ResponseWriter, r *http.Request) {
+	// Parse request body
+	var reqBody findNearestPrimeBodyRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		writeErrorJSONResponse(w, ErrPrimeInvalidInputFormat.Error(), http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	// validate inputs
+	if err := reqBody.validate(); err != nil {
+		writeErrorJSONResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Process the request
+	foundPrimeNumber, err := prime.FindLowerNearestPrimeNumberV2(reqBody.Num)
 	if err != nil {
 		writeErrorJSONResponse(w, err.Error(), http.StatusInternalServerError)
 		return
