cd44de3b0efe5b828c3c554d3aa644417b8f32b1
Phuong Hau <hvphuong98@gmail.com>
2021-05-24 09:31:17 +0700

    add test with big numbers for isPrimeNumberV2

--- pkg/service/prime/prime_test.go
+++ pkg/service/prime/prime_test.go
@@ -76,3 +76,37 @@ func TestFindLowerNearestPrimeNumber(t *testing.T) {
 		})
 	}
 }
+
+func TestFindLowerNearestPrimeNumberV2(t *testing.T) {
+	testName := func(input int, expected int) string {
+		if expected == -1 {
+			return fmt.Sprintf("There shouldn't be a number that is a prime number and also lower than %d", input)
+		}
+		return fmt.Sprintf("The highest prime number lower than %d should be: %d", input, expected)
+	}
+
+	cases := []struct {
+		input    int
+		expected int
+	}{
+		{input: 2, expected: -1},
+		{input: 1299827, expected: 1299821},
+		{input: 10000003957, expected: 10000003931},
+		{input: 100000039572313, expected: 100000039572263},
+		{input: 1000000395723132323, expected: 1000000395723132283},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.input, c.expected), func(t *testing.T) {
+			got, err := FindLowerNearestPrimeNumberV2(c.input)
+			if err != nil {
+				log.Fatalf("There shouldn't be an error, but one caught: %v", err.Error())
+			}
+			want := c.expected
+
+			if got != want {
+				log.Fatalf("Wanted: %v, got: %v", want, got)
+			}
+		})
+	}
+}
